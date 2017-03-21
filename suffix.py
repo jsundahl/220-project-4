98791dafc08816c49d0293f34587a5983d511202
jsundahl <jsundahl15@gmail.com>
2017-03-21 16:26:54 -0400

    fix fixing of add_word

--- suffix.py
+++ suffix.py
@@ -12,7 +12,7 @@ class Suffix:
             return suffix.put(word, 1)
         else:
             old_pair = suffix.get(word)
-            return suffix.put({old_pair[0]: old_pair[1] + 1})
+            return suffix.put(old_pair[0], old_pair[1] + 1)
 
     def choose_word(self, chain, prefix, randomizer):
         return NotImplemented
