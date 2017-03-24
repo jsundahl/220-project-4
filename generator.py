3da608f6b71f0961bddcbd9eec818b62ec54acb4
jsundahl <jsundahl15@gmail.com>
2017-03-24 13:29:55 -0400

    skeleton generator

---
+++ generator.py
@@ -0,0 +1,10 @@
+
+
+
+def get_word_list(chain, prefix, randomizer_fn, num_words, NONWORD):
+    raise NotImplementedError
+
+
+def generate(chain, randomizer_fn, num_words, NONWORD):
+    raise NotImplementedError
+
